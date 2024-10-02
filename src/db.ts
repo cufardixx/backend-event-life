00d01f26d102d16f32af60b6be315b311fc4cea5
cufardixx <facundopicia@gmail.com>
2024-10-02 19:57:44 -0300

    fix db

--- src/db.ts
+++ src/db.ts
@@ -7,19 +7,19 @@ import { env } from "process"
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
+    port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     entities: [User, Event, Ticket],
     logging: true,
-    synchronize: true
-})
+    synchronize: true,
+});
 
 AppDataSource.initialize()
     .then(() => {
-        console.log("Data Source has been initialized!")
+        console.log("Data Source has been initialized!");
     })
     .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
+        console.error("Error during Data Source initialization", err);
+    });
