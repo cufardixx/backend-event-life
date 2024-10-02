3f81a8fd2e540df0ce562c29dc2d14325867ceaf
cufardixx <facundopicia@gmail.com>
2024-10-02 15:20:26 -0300

    fix db

--- src/db.ts
+++ src/db.ts
@@ -2,14 +2,15 @@ import { DataSource } from "typeorm"
 import { User } from "./user/user.entity"
 import { Event } from "./event/event.entity"
 import { Ticket } from "./ticket/ticket.entity"
+import { env } from "process"
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "admin",
-    database: "typeormdb",
+    type: process.env.DB_TYPE as "postgres",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [User, Event, Ticket],
     logging: true,
     synchronize: true
