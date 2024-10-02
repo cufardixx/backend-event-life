ef82edcc408bac98c849770b1be4290ff3c12077
cufardixx <facundopicia@gmail.com>
2024-10-02 20:11:55 -0300

    fix db

--- src/db.ts
+++ src/db.ts
@@ -6,9 +6,9 @@ import { env } from "process"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     entities: [User, Event, Ticket],
