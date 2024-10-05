e7b00e3e82cb96d6bcb0e76535c066416073bbfe
cufardixx <facundopicia@gmail.com>
2024-10-05 10:29:56 -0300

    fix index

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
-import app from "./app";
-import AppDataSource  from "./db";
+import app from "./src/app";
+import AppDataSource  from "./src/db";
 import dotenv from "dotenv";
 
 dotenv.config();
